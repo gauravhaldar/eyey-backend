2b90782485b5a52c427b19e3a5aa7849f797561d
Batman@0001 <santrasuraj14@gmail.com>
2025-09-01 21:34:43 +0530

    update the cors preflight issue

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ app.use(
       "https://eyey-admin.vercel.app",
     ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   })
 );
 
